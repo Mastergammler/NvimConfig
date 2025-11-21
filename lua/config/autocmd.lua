c640c9ce5878a4826219dd17611b8637e55eba7b
Raphael Hoppe <r.hoppe@queo-group.com>
2025-11-21 15:59:27 +0100

    Setting auto cmd for blazor dev

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,7 +3,7 @@ local blazor = require("mg.test.blazor")
 local highlightGroup = vim.api.nvim_create_augroup("CustomHighlight", { clear = false })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedP", "TextChangedI" }, {
-    pattern = "*.blazor",
+    pattern = "*.razor",
     group = highlightGroup,
     callback = function()
         vim.defer_fn(function()
--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -1,7 +1,6 @@
 vim.filetype.add({
     extension = {
         template = "html",
-        razor = "html",
         props = "xml",
         csproj = "xml"
     }
